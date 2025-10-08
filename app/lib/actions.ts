d4dd2dac8b043cf99d040ce8eecf5fc13887cdf3
aditya <thakuriaditya121@gmail.com>
2025-10-08 08:39:11 +0545

    Implement server-side validation with useActionState hook and detailed error handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,7 +6,8 @@ import { redirect } from 'next/navigation';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
-const CreateInvoice = z.object({
+const FormSchema = z.object({
+  id: z.string(),
   customerId: z.string({
     invalid_type_error: 'Please select a customer.',
   }),
@@ -16,10 +17,22 @@ const CreateInvoice = z.object({
   status: z.enum(['pending', 'paid'], {
     invalid_type_error: 'Please select an invoice status.',
   }),
+  date: z.string(),
 });
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 // Use Zod to update the expected types
 const UpdateInvoice = CreateInvoice;
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 export type Invoice = {
   id: string; // Will be created on the database
   customer_id: string;
@@ -27,27 +40,41 @@ export type Invoice = {
   status: 'pending' | 'paid';
   date: string;
 };
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
  
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+ 
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
  
+  // Insert data into the database
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
   } catch (error) {
-    // We'll also log the error to the console for now
-    console.error('Database Error:', error);
-    throw new Error('Failed to Create Invoice.');
+    // If a database error occurs, return a more specific error.
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
   }
  
+  // Revalidate the cache for the invoices page and redirect the user.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
